cc45dff3fbcfa7082952eac6d35bcbe007c69f29
basit-GOK <basit.ahmed@geeksofkolachi.com>
2026-01-08 17:46:31 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_ec2():
-    return 'Welcome to EC2'
+    return 'Welcome to '
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
